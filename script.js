d17a94f71e371e1e1627e51a5310452a67b83709
LuisLeonardobs <luisleonardobs@outlook.com>
2025-04-26 11:57:57 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let sliderElement = document.querySelector("#slider");
+let butonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerpassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let novasenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+    
+}
+
+function generatepassword(){
+    
+    let pass = "";
+    for(let i= 0, n=charset.length; i< sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+
+    containerpassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novasenha = pass;
+}
+
+function copyPassword(){
+    alert("SENHA COPIADA COM SUCESSO!")
+    navigator.clipboard.writeText(novasenha);
+}
+
